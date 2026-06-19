768ad5343837da2f2225d5e1d629457fb6a94cc7
Soshi E <under0thxpickluck@gmail.com>
2026-06-19 16:05:31 +0900

    fix: remove next-intl/use-intl from transpilePackages to fix Edge middleware crash

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,5 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts')
 
 export default withNextIntl({
-  transpilePackages: ['next-intl', 'use-intl', 'intl-messageformat', '@formatjs/fast-memoize', '@formatjs/icu-messageformat-parser', '@formatjs/icu-skeleton-parser', '@formatjs/intl-localematcher'],
+  transpilePackages: ['intl-messageformat', '@formatjs/fast-memoize', '@formatjs/icu-messageformat-parser', '@formatjs/icu-skeleton-parser', '@formatjs/intl-localematcher'],
 })
